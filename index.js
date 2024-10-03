81316a333f8e3931f2f80bf075c01711417d50ae
AndresPatino <andrespatino0845@gmail.com>
2024-10-03 14:21:35 -0500

    Exercises 3.1-3.6

--- index.js
+++ index.js
@@ -1,9 +1,88 @@
 const express = require('express')
 const app = express()
 app.use(express.json())//Para acceder a los datos fácilmente, necesitamos la ayuda del json-parser de Express, que se usa con el comando 
-
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
+  })
+
+  app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+ 
+app.get('/api/info', (request, response) => {
+    const date = new Date()
+    response.send(`<p>Phonebook has info for <strong>${persons.length}</strong> people</p><p>${date}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (persons) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+const generateId = () => {
+    return Math.floor(Math.random() * 1000000)
+}
+  app.post('/api/persons', (request, response) => {
+    const body = request.body
+  
+    if (!body.name || !body.number) {
+      return response.status(400).json({ 
+        error: 'name or number missing' 
+      })
+    }
+
+    if(body.name.trim() === ""){
+        return response.status(400).json({ 
+            error: 'name is empty' 
+          })
+    }
+    if(persons.some(person => person.name === body.name)){
+        return response.status(400).json({ 
+            error: 'name must be unique' 
+          })
+    }
+    const person = {
+      name: body.name,
+      number: body.number,
+      id: generateId(),
+    }
+  
+    persons = persons.concat(person)
+  
+    response.json(person)
+  })
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+  
+    response.status(204).end()
   })
 
 
